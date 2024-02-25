33b46c434bea6176fe90a8d004e725a625fcf2d2
rbangre2 <rbangre2@gmail.com>
2024-02-25 12:29:45 -0500

    seperated dashboard layout

--- src/app/dashboard/home/page.tsx
+++ src/app/dashboard/home/page.tsx
@@ -1,37 +1,16 @@
 "use client";
-import React, { useState } from "react";
-import { CssBaseline, Box } from "@mui/material";
-import Header from "@/components/Header/Header";
-import AppDrawer from "@/components/AppDrawer/AppDrawer";
-
-const appBarHeight = 65;
-const drawerWidth = 240;
-const minimizedDrawerWidth = 56;
+import React from "react";
+import DashboardLayout from "../DashboardLayout";
+import Typography from "@mui/material/Typography";
 
 const HomePage: React.FC = () => {
-  const [drawerOpen, setDrawerOpen] = useState<boolean>(true);
-
-  const handleDrawerOpen = (): void => {
-    setDrawerOpen(true);
-  };
-
-  const handleDrawerClose = (): void => {
-    setDrawerOpen(false);
-  };
-
   return (
-    <Box sx={{ display: "flex" }}>
-      <CssBaseline />
-      <Header handleDrawerOpen={handleDrawerOpen} drawerOpen={drawerOpen} />
-      <AppDrawer
-        open={drawerOpen}
-        handleDrawerClose={handleDrawerClose}
-        drawerWidth={drawerWidth}
-        minimizedDrawerWidth={minimizedDrawerWidth}
-        appBarHeight={appBarHeight}
-      />
-      {/* Content here */}
-    </Box>
+    <DashboardLayout>
+      <Typography variant="h4" gutterBottom>
+        Home Page Content
+      </Typography>
+      {/* Add the rest of your home page content here */}
+    </DashboardLayout>
   );
 };
 
