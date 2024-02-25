ad8e2af0a8209b3720873985f3cf2141a9c04ab5
rbangre2 <rbangre2@gmail.com>
2024-02-25 12:10:41 -0500

    seperated header component

--- src/app/dashboard/home/page.tsx
+++ src/app/dashboard/home/page.tsx
@@ -1,59 +1,28 @@
 "use client";
 import React, { useState } from "react";
-import {
-  AppBar,
-  Toolbar,
-  IconButton,
-  Typography,
-  CssBaseline,
-  Box,
-} from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
+import { CssBaseline, Box } from "@mui/material";
+import Header from "@/components/Header/Header";
 import AppDrawer from "@/components/AppDrawer/AppDrawer";
-import styles from "./page.module.css";
 
-const appBarHeight = 65; // The height of your AppBar, adjust as needed
+const appBarHeight = 65;
 const drawerWidth = 240;
 const minimizedDrawerWidth = 56;
 
 const HomePage: React.FC = () => {
   const [drawerOpen, setDrawerOpen] = useState<boolean>(true);
 
+  const handleDrawerOpen = (): void => {
+    setDrawerOpen(true);
+  };
+
   const handleDrawerClose = (): void => {
-    setDrawerOpen(!drawerOpen);
+    setDrawerOpen(false);
   };
 
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
-      <AppBar
-        position="fixed"
-        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
-      >
-        <Toolbar sx={{ backgroundColor: "#1A1B1E" }}>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerClose}
-            sx={{ marginRight: "36px" }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            variant="h6"
-            noWrap
-            style={{
-              fontFamily: "Alex Brush, cursive",
-              fontWeight: 400,
-              fontSize: "3rem",
-              fontStyle: "normal",
-            }}
-          >
-            wagr
-          </Typography>
-        </Toolbar>
-      </AppBar>
+      <Header handleDrawerOpen={handleDrawerOpen} drawerOpen={drawerOpen} />
       <AppDrawer
         open={drawerOpen}
         handleDrawerClose={handleDrawerClose}
