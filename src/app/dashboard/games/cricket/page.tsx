6a169ff765a07a4ef00d760d3ac4bf890f4f9605
Raffay Shahid <raffayshahid@hotmail.com>
2024-02-26 01:06:48 -0500

    Added Cricket to the Sports Page and dashboard

---
+++ src/app/dashboard/games/cricket/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import GamesLayout from "../GamesLayout";
+
+export default function CricketGames() {
+  return (
+    <GamesLayout>
+      <div> Cricket games</div>{" "}
+    </GamesLayout>
+  );
+}
