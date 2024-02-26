f5aa08235d538679859aad597a03040c62afc68d
rbangre2 <rbangre2@gmail.com>
2024-02-26 18:40:32 -0500

    friends ui with fake data

--- src/app/dashboard/friends/page.tsx
+++ src/app/dashboard/friends/page.tsx
@@ -1,10 +1,32 @@
 "use client";
 import DashboardLayout from "../DashboardLayout";
+import { Box, Typography } from "@mui/material";
+import styles from "./page.module.css";
+import FriendsTable from "@/components/FriendsTable/FriendsTable";
+import AddFriend from "@/components/AddFriend/AddFriend";
 
 export default function Friends() {
   return (
     <DashboardLayout>
-      <div> friends </div>
+      <Box className={styles.friendsContainer}>
+        <Box className={styles.leftSide}>
+          <Typography
+            variant="h3"
+            component="h1"
+            sx={{
+              marginBottom: "20px",
+              fontFamily: "'PT Sans Narrow', sans-serif",
+              fontWeight: 700,
+            }}
+          >
+            Friends!
+          </Typography>
+          <FriendsTable />
+        </Box>
+        <Box className={styles.rightSide}>
+          <AddFriend />
+        </Box>
+      </Box>
     </DashboardLayout>
   );
 }
