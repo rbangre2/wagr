df13fb292ec70d3fa1dfa52c7d725aaaab0935a0
rbangre2 <rbangre2@gmail.com>
2024-02-26 22:53:50 -0500

    filter row for bets page

---
+++ src/components/FilterRow/FilterRow.tsx
@@ -0,0 +1,123 @@
+import React, { useState } from "react";
+import {
+  Box,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Button,
+  Grid,
+  TextField,
+} from "@mui/material";
+import OddsRangeSlider from "../OddsSlider/OddsSlider";
+import { Sport, League } from "./types";
+
+const FilterRow: React.FC = () => {
+  const [selectedSport, setSelectedSport] = useState<Sport | "">("");
+  const [selectedLeague, setSelectedLeague] = useState<League | "">("");
+  const [oddsRange, setOddsRange] = useState<number[]>([1.01, 5]);
+  const [amount, setAmount] = useState<number | "">("");
+
+  const handleApplyFilters = () => {
+    console.log(selectedSport, selectedLeague, oddsRange, amount);
+  };
+
+  const handleOddsRangeChange = (event: Event, newValue: number | number[]) => {
+    setOddsRange(newValue as number[]);
+  };
+
+  return (
+    <Box
+      sx={{
+        flexGrow: 1,
+        marginBottom: 2,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+      }}
+    >
+      <Grid container spacing={2} alignItems="center">
+        <Grid item xs={2}>
+          <FormControl fullWidth>
+            <InputLabel id="sport-select-label">Sport</InputLabel>
+            <Select
+              labelId="sport-select-label"
+              id="sport-select"
+              value={selectedSport}
+              label="Sport"
+              onChange={(event) =>
+                setSelectedSport(event.target.value as Sport)
+              }
+            >
+              {Object.values(Sport).map((sport) => (
+                <MenuItem key={sport} value={sport}>
+                  {sport}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={2}>
+          <FormControl fullWidth>
+            <InputLabel id="league-select-label">League</InputLabel>
+            <Select
+              labelId="league-select-label"
+              id="league-select"
+              value={selectedLeague}
+              label="League"
+              onChange={(event) =>
+                setSelectedLeague(event.target.value as League)
+              }
+            >
+              {Object.values(League).map((league) => (
+                <MenuItem key={league} value={league}>
+                  {league}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={4}>
+          <Box
+            sx={{
+              marginTop: "20px",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <OddsRangeSlider
+              value={oddsRange}
+              onChange={handleOddsRangeChange}
+              valueLabelDisplay="auto"
+              min={1.01}
+              max={5}
+              step={0.01}
+            />
+          </Box>
+        </Grid>
+        <Grid item xs={2}>
+          <TextField
+            id="amount-input"
+            label="Amount"
+            type="number"
+            variant="outlined"
+            fullWidth
+            value={amount}
+            onChange={(event) => {
+              const value = event.target.value;
+              const numericalValue = parseFloat(value);
+              setAmount(numericalValue >= 0 ? numericalValue : "");
+            }}
+          />
+        </Grid>
+        <Grid item xs={2}>
+          <Button variant="contained" onClick={handleApplyFilters} fullWidth>
+            Apply Filters
+          </Button>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default FilterRow;
---
+++ src/components/FilterRow/types.ts
@@ -0,0 +1,49 @@
+export enum Sport {
+  Soccer = "Soccer",
+  Basketball = "Basketball",
+  Hockey = "Hockey",
+  Cricket = "Cricket",
+}
+
+export enum League {
+  EPL = "English Premier League",
+  LaLiga = "La Liga",
+  SerieA = "Serie A",
+  Bundesliga = "Bundesliga",
+  NBA = "National Basketball Association",
+  Euroleague = "Euroleague",
+  NHL = "National Hockey League",
+  ICL = "Indian Cricket League",
+}
+
+export type FilterSportsOption = {
+  label: string;
+  value: Sport;
+};
+
+export type FilterLeagueOption = {
+  label: string;
+  sport: Sport;
+  value: League;
+};
+
+export const sportsOptions: FilterSportsOption[] = [
+  { label: Sport.Soccer, value: Sport.Soccer },
+  { label: Sport.Basketball, value: Sport.Basketball },
+  { label: Sport.Hockey, value: Sport.Hockey },
+  { label: Sport.Cricket, value: Sport.Cricket },
+];
+
+export const leagueOptions: FilterLeagueOption[] = [
+  { label: League.NBA, sport: Sport.Basketball, value: League.NBA },
+  { label: League.EPL, sport: Sport.Soccer, value: League.EPL },
+  { label: League.Bundesliga, sport: Sport.Soccer, value: League.Bundesliga },
+  {
+    label: League.Euroleague,
+    sport: Sport.Basketball,
+    value: League.Euroleague,
+  },
+  { label: League.SerieA, sport: Sport.Soccer, value: League.SerieA },
+  { label: League.LaLiga, sport: Sport.Soccer, value: League.LaLiga },
+  { label: League.NHL, sport: Sport.Hockey, value: League.NHL },
+];
